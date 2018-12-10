f3b36815607a64ca94bcc64bc8c4620f96ad31eb
s-nandi <saujas.nandi@gmail.com>
2018-12-10 17:40:03 -0500

    Change DCEL struct to be a class to hide implementation details

--- include/DCEL.h
+++ include/DCEL.h
@@ -8,8 +8,9 @@ struct face;
 struct vertex;
 struct halfedge;
 
-struct DCEL
+class DCEL
 {
+private:  
     struct endpoint_indices
     {
         std::pair <int, int> indices;
@@ -30,11 +31,12 @@ struct DCEL
     halfedge* getNewEdge();
     face* getNewFace(int id = 0);
 
-    void buildFromTriangles(std::vector <pt> &points, std::vector <std::vector<int>> &triangles);
     void createPointSet(std::vector <pt> &points);
     void buildFaces(std::vector <std::vector<int>> &triangles, std::vector <endpoint_indices> &created_edges);
     void matchTwinEdges(std::vector <endpoint_indices> &created_edges, std::vector <endpoint_indices> &twinless_edges);
     void buildExterior(std::vector <endpoint_indices> &twinless_edges);
+public:
+    void buildFromTriangles(std::vector <pt> &points, std::vector <std::vector<int>> &triangles);
 };
 
 #endif // DCEL_H_DEFINED
