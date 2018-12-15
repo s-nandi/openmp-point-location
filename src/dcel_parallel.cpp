20a905177106b80a961ccf0cecc3e90ba0a49998
snandi <saujas.nandi@gmail.com>
2018-12-15 16:19:30 -0500

    Undo accidental commenting out of file

--- src/dcel_parallel.cpp
+++ src/dcel_parallel.cpp
@@ -29,63 +29,63 @@ void DCEL::parallel_createPointSet(std::vector <pt> &points)
     }
 }
 
-void DCEL::parallel_buildFaces(std::vector <std::vector<int>> &triangles, std::vector <endpoint_indi// ces> &created_edges)
-// {
-//     faces.resize(triangles.size());
-//     edges.resize(3 * triangles.size());
-//     created_edges.resize(3 * triangles.size());
-//     #pragma omp parallel for
-//     for (int i = 0; i < triangles.size(); i++)
-//     {
-// 	int indices[3] = {triangles[i][0], triangles[i][1], triangles[i][2]};
+void DCEL::parallel_buildFaces(std::vector <std::vector<int>> &triangles, std::vector <endpoint_indices> &created_edges)
+{
+    faces.resize(triangles.size());
+    edges.resize(3 * triangles.size());
+    created_edges.resize(3 * triangles.size());
+    #pragma omp parallel for
+    for (int i = 0; i < triangles.size(); i++)
+    {
+	int indices[3] = {triangles[i][0], triangles[i][1], triangles[i][2]};
 
-// 	auto orient = pt::orientation(vertices[indices[0]] -> point, vertices[indices[1]] -> point, vertices[indices[2]] -> point);
-// 	if (orient > 0) // Ensure that half edges in a face are ccw oriented
-// 	{
-// 	    std::swap(indices[0], indices[2]);
-// 	}
+	auto orient = pt::orientation(vertices[indices[0]] -> point, vertices[indices[1]] -> point, vertices[indices[2]] -> point);
+	if (orient > 0) // Ensure that half edges in a face are ccw oriented
+	{
+	    std::swap(indices[0], indices[2]);
+	}
 
-// 	faces[i] = new face(i);
-// 	face* curr_face = faces[i];
+	faces[i] = new face(i);
+	face* curr_face = faces[i];
 	
-// 	halfedge* triangle_edges[3]; // j-th half edge originates from indices[j]
-// 	for (int j = 0; j < 3; j++)
-// 	{
-// 	    triangle_edges[j] = new halfedge;
-// 	    edges[3 * i + j] = triangle_edges[j];
-// 	}
+	halfedge* triangle_edges[3]; // j-th half edge originates from indices[j]
+	for (int j = 0; j < 3; j++)
+	{
+	    triangle_edges[j] = new halfedge;
+	    edges[3 * i + j] = triangle_edges[j];
+	}
 
-// 	for (int j = 0; j < 3; j++)
-// 	{
-// 	    int jnext = (j + 1) % 3;
+	for (int j = 0; j < 3; j++)
+	{
+	    int jnext = (j + 1) % 3;
 
-// 	    triangle_edges[j] -> setIncidentFace(curr_face); // Every edge belongs to same face
-// 	    triangle_edges[j] -> setOrigin(vertices[indices[j]]);// Set origin of halfedge and incidentEdge for origin
-// 	    triangle_edges[j] -> setNext(triangle_edges[jnext]); // Set next edge for face traversal
+	    triangle_edges[j] -> setIncidentFace(curr_face); // Every edge belongs to same face
+	    triangle_edges[j] -> setOrigin(vertices[indices[j]]);// Set origin of halfedge and incidentEdge for origin
+	    triangle_edges[j] -> setNext(triangle_edges[jnext]); // Set next edge for face traversal
 
-// 	    created_edges[3 * i + j] = endpoint_indices(triangles[i][j], triangles[i][jnext], edges[3 * i + j]);
-// 	}
-//     }
-// }
+	    created_edges[3 * i + j] = endpoint_indices(triangles[i][j], triangles[i][jnext], edges[3 * i + j]);
+	}
+    }
+}
 
-// void DCEL::parallel_matchTwinEdges(std::vector <endpoint_indices> &created_edges, std::vector <endpoint_indices> &twinless_edges)
-// {
-//     auto radix_sort = [](std::vector <endpoint_indices> &vp, int numPoints)
-// 		      {
-// 			  auto getSecond = [](endpoint_indices vp){return vp.indices.second;};
-// 			  bucket_sort(vp, numPoints, getSecond);
-// 			  auto getFirst = [](endpoint_indices vp){return vp.indices.first;};
-// 			  bucket_sort(vp, numPoints, getFirst);
-// 		      };
-//     radix_sort(created_edges, vertices.size());
+void DCEL::parallel_matchTwinEdges(std::vector <endpoint_indices> &created_edges, std::vector <endpoint_indices> &twinless_edges)
+{
+    auto radix_sort = [](std::vector <endpoint_indices> &vp, int numPoints)
+		      {
+			  auto getSecond = [](endpoint_indices vp){return vp.indices.second;};
+			  bucket_sort(vp, numPoints, getSecond);
+			  auto getFirst = [](endpoint_indices vp){return vp.indices.first;};
+			  bucket_sort(vp, numPoints, getFirst);
+		      };
+    radix_sort(created_edges, vertices.size());
     
-//     for (int i = 0; i < created_edges.size(); )
-//     {
-// 	if (i + 1 < created_edges.size() and created_edges[i].indices == created_edges[i + 1].indices)
-// 	{
-// 	    created_edges[i].edge -> setTwin(created_edges[i + 1].edge);
-// 	    i += 2;
-// 	}
+    for (int i = 0; i < created_edges.size(); )
+    {
+	if (i + 1 < created_edges.size() and created_edges[i].indices == created_edges[i + 1].indices)
+	{
+	    created_edges[i].edge -> setTwin(created_edges[i + 1].edge);
+	    i += 2;
+	}
 	else
 	{
 	    twinless_edges.push_back(created_edges[i]);
